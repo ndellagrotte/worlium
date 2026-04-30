904d2035be580290140eb2697862050284bdb928
Nicholas DellaGrotte <ndell1@unh.newhaven.edu>
2026-04-30 01:59:13 -0400

    Revert "biome-based AirOnlyAquifer impl"
    This reverts commit 5867fa6cc72c1db61a5e26260ae998b3ace403b8.

--- common/src/main/java/com/denizen/worlium/mixin/NoiseGeneratorSettingsMixin.java
+++ common/src/main/java/com/denizen/worlium/mixin/NoiseGeneratorSettingsMixin.java
@@ -40,6 +40,7 @@ public abstract class NoiseGeneratorSettingsMixin implements WorliumModifiedNgs 
         WorldSeedHolder.LOADING_OVERWORLD_NGS.remove();
 
         NoiseRouter old = this.noiseRouter;
+        WorleyDensityFunction.INSTANCE.setOceanGate(old.continents());
         this.noiseRouter = new NoiseRouter(
             old.barrierNoise(),
             old.fluidLevelFloodednessNoise(),
